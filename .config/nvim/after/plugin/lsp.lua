19fa6d42890beb6016ff1630fa237d6961bd53f6
Jonathan Ho <jonwho.contact@gmail.com>
2024-07-05 13:39:49 -0700

    config formatter to format on write

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -1,55 +1,57 @@
 -- Order of LSP setup matters.
 
 -- Mason first
-require('mason').setup({})
-require('mason-lspconfig').setup({
-  ensure_installed = {
-    'lua_ls',
-    'rubocop',
-    'ruby_lsp',
-    'gopls',
-  },
+require("mason").setup({})
+require("mason-lspconfig").setup({
+	ensure_installed = {
+		"lua_ls",
+		"rubocop",
+		"ruby_lsp",
+		"gopls",
+	},
 })
 
 -- Configure LSP after Mason setup
-local lsp_zero = require('lsp-zero')
+local lsp_zero = require("lsp-zero")
 
 -- lsp_attach is where you enable features that only work
 -- if there is a language server active in the file
 local lsp_attach = function(client, bufnr)
-  local opts = {buffer = bufnr}
+	local opts = { buffer = bufnr }
 
-  vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
-  vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
-  vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
-  vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
-  vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
-  vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
-  vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
-  vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
-  vim.keymap.set({'n', 'x'}, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
-  vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
+	vim.keymap.set("n", "K", "<cmd>lua vim.lsp.buf.hover()<cr>", opts)
+	vim.keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<cr>", opts)
+	vim.keymap.set("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<cr>", opts)
+	vim.keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<cr>", opts)
+	vim.keymap.set("n", "go", "<cmd>lua vim.lsp.buf.type_definition()<cr>", opts)
+	vim.keymap.set("n", "gr", "<cmd>lua vim.lsp.buf.references()<cr>", opts)
+	vim.keymap.set("n", "gs", "<cmd>lua vim.lsp.buf.signature_help()<cr>", opts)
+	vim.keymap.set("n", "<F2>", "<cmd>lua vim.lsp.buf.rename()<cr>", opts)
+	vim.keymap.set({ "n", "x" }, "<F3>", "<cmd>lua vim.lsp.buf.format({async = true})<cr>", opts)
+	vim.keymap.set("n", "<F4>", "<cmd>lua vim.lsp.buf.code_action()<cr>", opts)
+	vim.keymap.set("n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<cr>", opts)
 end
 
 lsp_zero.extend_lspconfig({
-  sign_text = true,
-  lsp_attach = lsp_attach,
-  capabilities = require('cmp_nvim_lsp').default_capabilities(),
+	sign_text = true,
+	lsp_attach = lsp_attach,
+	capabilities = require("cmp_nvim_lsp").default_capabilities(),
 })
 
-require('lspconfig').lua_ls.setup({})
-require('lspconfig').ruby_lsp.setup({})
-require('lspconfig').gopls.setup({})
-require('lspconfig').tailwindcss.setup({})
-require('lspconfig').bashls.setup({})
-require('lspconfig').tsserver.setup({})
-require('lspconfig').sqls.setup({})
-require('lspconfig').theme_check.setup({})
-require('lspconfig').vuels.setup({})
-require('lspconfig').yamlls.setup({})
-require('lspconfig').html.setup({})
-require('lspconfig').dockerls.setup({})
-require('lspconfig').docker_compose_language_service.setup({})
-require('lspconfig').cssls.setup({})
-require('lspconfig').bufls.setup({})
-require('lspconfig').templ.setup({})
+require("lspconfig").lua_ls.setup({})
+require("lspconfig").rubocop.setup({})
+require("lspconfig").ruby_lsp.setup({})
+require("lspconfig").gopls.setup({})
+require("lspconfig").tailwindcss.setup({})
+require("lspconfig").bashls.setup({})
+require("lspconfig").tsserver.setup({})
+require("lspconfig").sqls.setup({})
+require("lspconfig").theme_check.setup({})
+require("lspconfig").vuels.setup({})
+require("lspconfig").yamlls.setup({})
+require("lspconfig").html.setup({})
+require("lspconfig").dockerls.setup({})
+require("lspconfig").docker_compose_language_service.setup({})
+require("lspconfig").cssls.setup({})
+require("lspconfig").bufls.setup({})
+require("lspconfig").templ.setup({})
